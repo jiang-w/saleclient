0c0abfcbe40144bb01b014aa127344800c9ed22f
jiang-w <frank@iMac-bigdata.local>
2015-12-25 14:17:41 +0800

    commit

---
+++ saleclient/QRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCodeViewController.h
+//  saleclient
+//
+//  Created by Frank on 15/12/25.
+//  Copyright © 2015年 oceano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRCodeViewController : UIViewController
+
+@end
---
+++ saleclient/QRCodeViewController.m
@@ -0,0 +1,96 @@
+//
+//  QRCodeViewController.m
+//  saleclient
+//
+//  Created by Frank on 15/12/25.
+//  Copyright © 2015年 oceano. All rights reserved.
+//
+
+#import "QRCodeViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface QRCodeViewController () <AVCaptureMetadataOutputObjectsDelegate, UIAlertViewDelegate>
+
+@property (strong, nonatomic) UIView                     *scanRectView;
+@property (strong, nonatomic) AVCaptureDevice            *device;
+@property (strong, nonatomic) AVCaptureDeviceInput       *input;
+@property (strong, nonatomic) AVCaptureMetadataOutput    *output;
+@property (strong, nonatomic) AVCaptureSession           *session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *preview;
+
+@end
+
+@implementation QRCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupCamera];
+}
+
+- (void)setupCamera {
+    CGSize windowSize = [UIScreen mainScreen].bounds.size;
+    
+    CGSize scanSize = CGSizeMake(windowSize.width*3/4, windowSize.width*3/4);
+    CGRect scanRect = CGRectMake((windowSize.width-scanSize.width)/2, (windowSize.height-scanSize.height)/2,
+                                 scanSize.width, scanSize.height);
+    //计算rectOfInterest 如果是竖屏x,y交换位置
+    scanRect = CGRectMake(scanRect.origin.x/windowSize.width, scanRect.origin.y/windowSize.height,
+                          scanRect.size.width/windowSize.width, scanRect.size.height/windowSize.height);
+    
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    
+    self.output = [[AVCaptureMetadataOutput alloc]init];
+    [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    self.session = [[AVCaptureSession alloc]init];
+    [self.session setSessionPreset:([UIScreen mainScreen].bounds.size.height<500)?AVCaptureSessionPreset640x480:AVCaptureSessionPresetHigh];
+    [self.session addInput:self.input];
+    [self.session addOutput:self.output];
+    self.output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode];
+    self.output.rectOfInterest = scanRect;
+    
+    self.preview = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    self.preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    self.preview.frame = [UIScreen mainScreen].bounds;
+    [self.view.layer insertSublayer:self.preview atIndex:0];
+    
+    self.scanRectView = [UIView new];
+    [self.view addSubview:self.scanRectView];
+    self.scanRectView.frame = CGRectMake(0, 0, scanSize.width, scanSize.height);
+    self.scanRectView.center = CGPointMake(CGRectGetMidX([UIScreen mainScreen].bounds), CGRectGetMidY([UIScreen mainScreen].bounds));
+    self.scanRectView.layer.borderColor = [UIColor redColor].CGColor;
+    self.scanRectView.layer.borderWidth = 1;
+    
+    
+    //开始捕获
+    [self.session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    if ( (metadataObjects.count == 0) ) {
+        return;
+    }
+    
+    if (metadataObjects.count > 0) {
+        
+        [self.session stopRunning];
+        
+        AVMetadataMachineReadableCodeObject *metadataObject = metadataObjects.firstObject;
+        
+        //输出扫描字符串
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:metadataObject.stringValue message:@"" delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+        
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView willDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    [self.session startRunning];
+}
+
+
+@end
