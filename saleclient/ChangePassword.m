f1bcbfe4e1892f803ebece8737b0a1c033e1682b
jiang-w <jiang.w@live.com>
2016-03-09 22:46:41 +0800

    commit

--- saleclient/ChangePassword.m
+++ saleclient/ChangePassword.m
@@ -10,6 +10,7 @@
 #import "UIViewController+LewPopupViewController.h"
 #import<CommonCrypto/CommonDigest.h>
 #import "OSNUserManager.h"
+#import "UIResponder+FirstResponder.h"
 
 @interface ChangePassword ()
 
@@ -17,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *currentPassword;
 @property (weak, nonatomic) IBOutlet UITextField *editPassword;
 @property (weak, nonatomic) IBOutlet UITextField *checkPassword;
+@property (nonatomic, assign) CGRect originalFrame;
 
 
 @end
@@ -27,22 +29,39 @@
     self = [super initWithFrame:frame];
     if (self) {
         [[NSBundle mainBundle] loadNibNamed:[[self class] description] owner:self options:nil];
-        _innerView.frame = frame;
+        _innerView.frame = CGRectMake(302, 244, 420, 280);
         _innerView.layer.borderWidth = 2;
         _innerView.layer.borderColor = [UIColor orangeColor].CGColor;
         [self addSubview:_innerView];
+        
+        _originalFrame = _innerView.frame;
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(actionKeyboardShow:)
+                                                     name:UIKeyboardDidShowNotification
+                                                   object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(actionKeyboardHide:)
+                                                     name:UIKeyboardWillHideNotification
+                                                   object:nil];
     }
     return self;
 }
 
 + (instancetype)defaultView {
-    ChangePassword *view = [[ChangePassword alloc]initWithFrame:CGRectMake(0, 0, 420, 280)];
+    ChangePassword *view = [[ChangePassword alloc]initWithFrame:[UIScreen mainScreen].bounds];
     return view;
 }
 
 - (IBAction)submitButtonClick:(id)sender {
+    [self endEditing:YES];
     if (![self.editPassword.text isEqualToString:self.checkPassword.text]) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"两次输入的密码不一致" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    if (IS_EMPTY_STRING(self.editPassword.text)) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"密码不能为空" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
         [alert show];
         return;
     }
@@ -98,6 +117,61 @@
     }
     
     return output;
+}
+
+
+#pragma mark - notification
+
+- (void)actionKeyboardShow:(NSNotification *)notification {
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    id responder = [UIResponder currentFirstResponder];
+    if (responder && [responder isKindOfClass:[UITextField class]]) {
+        UITextField *txtField = (UITextField *)responder;
+        CGRect txtFrame = txtField.frame;
+        
+        CGFloat yOffset = self.innerView.frame.size.height - txtFrame.origin.y - txtFrame.size.height - keyboardSize.height;
+        if (yOffset < 0) {
+            [UIView beginAnimations:@"ResizeForKeyBoard" context:nil];
+            [UIView setAnimationDuration:0.3];
+            float width = self.innerView.frame.size.width;
+            float height = self.innerView.frame.size.height;
+            CGRect rect = CGRectMake(self.innerView.frame.origin.x, self.innerView.frame.origin.x - 200, width, height);
+            self.innerView.frame = rect;
+            [UIView commitAnimations];
+        }
+    }
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(actionKeyboardShow:)
+                                                 name:UIKeyboardDidChangeFrameNotification
+                                               object:nil];
+}
+
+- (void)actionKeyboardHide:(NSNotification *)notification {
+    id responder = [UIResponder currentFirstResponder];
+    if (responder && [responder isKindOfClass:[UITextField class]]) {
+        UITextField *txtField = (UITextField *)responder;
+        [txtField resignFirstResponder];
+    }
+    
+    [UIView beginAnimations:@"ResizeForKeyBoard" context:nil];
+    [UIView setAnimationDuration:0.3];
+    self.innerView.frame = self.originalFrame;
+    [UIView commitAnimations];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(actionKeyboardShow:)
+                                                 name:UIKeyboardDidShowNotification
+                                               object:nil];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
 @end
